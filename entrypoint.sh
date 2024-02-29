fc886a414521ac0386f07e78dda46e5892fd17a3
OutEastCheshire <winampagent007@mail.ru>
2024-02-29 12:15:16 +0300

    entrypoint func

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+
+flake8 .
+
 python -m unittest discover tests
 
-python app.py
+sleep 10
+
+pkill -f "python app.py"
